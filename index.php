1126d90ffb2946f637cf7f20e6ea798b47d18b0a
Vishal Rabade <118335291+Vishalrabade@users.noreply.github.com>
2024-07-10 23:24:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
         <p class=" w3l-register-p">Don't have an account?<a href="signup.php" class="register"> Sign up</a></p>
     </div>
     <footer>
-        <p class="copyright-agileinfo"> &copy; Yash Awachare. All Rights Reserved</p>
+        <p class="copyright-agileinfo"> &copy; HostelMS. All Rights Reserved</p>
     </footer>
 
 </body>
