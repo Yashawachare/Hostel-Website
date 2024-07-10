7abe3a91a6aa8e4749d2e74259125f6f5c82a40a
Yash Shailendra Awachare <107410146+Yashawachare@users.noreply.github.com>
2024-07-10 23:13:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -45,11 +45,11 @@
                     <input type="password" class="form-control" name="pwd" placeholder="Password" required="required" />
                 </div>
             </div>
-            <!-- <div class="forgot">
+            <button type="submit" name="login-submit">Login</button>
+            <div class="forgot">
                 <a href="#">Forgot Password?</a>
                 <p><input type="checkbox">Remember Me</p>
-            </div> -->
-            <button type="submit" name="login-submit">Login</button>
+            </div>
         </form>
           <p class=" w3l-register-p">Login as<a href="login-hostel_manager.php" class="register"> Hostel-Manager/Admin</a></p>
         <p class=" w3l-register-p">Don't have an account?<a href="signup.php" class="register"> Sign up</a></p>
