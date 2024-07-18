d3814b6104aca4870e7d098cd6535e626534eac1
rupprashik <113437186+rupprashik@users.noreply.github.com>
2024-07-18 23:55:46 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -29,13 +29,15 @@
     <h1>Registration</h1>
     <div class=" w3l-login-form">
         <h2>Sign Up Here</h2>
-        <form action="includes/signup.inc.php" method="POST">
+
+        
+        <form action="registration.php" method="POST">
 
             <div class=" w3l-form-group">
                 <label>Aadhar Number:</label>
                 <div class="group">
                     <i class="fas fa-id-badge"></i>
-                    <input type="text" class="form-control" name="" placeholder="" required="required" />
+                    <input type="text" class="form-control" name="aadhar" placeholder="" required="required" />
                 </div>
             </div>
 
@@ -43,7 +45,7 @@
                 <label>Name as per Aadhar:</label>
                 <div class="group">
                     <i class="fas fa-id-badge"></i>
-                    <input type="text" class="form-control" name="" placeholder="" required="required" />
+                    <input type="text" class="form-control" name="name" placeholder="" required="required" />
                 </div>
             </div>
 
@@ -51,16 +53,37 @@
                 <label>Mobile Number:</label>
                 <div class="group">
                     <i class="fas fa-id-badge"></i>
-                    <input type="text" class="form-control" name="" placeholder="" required="required" />
+                    <input type="text" class="form-control" name="mob" placeholder="" required="required" />
                 </div>
             </div>
             
+            <div class=" w3l-form-group">
+                <label>Date of birth:</label>
+                <div class="group">
+                    <i class="fas fa-id-badge"></i>
+                    <input type="date" class="form-control" name="dob" placeholder="" required="required" />
+                </div>
+            </div>
+
+            <div class="w3l-form-group">
+                <label>Gender:</label>
+                <div class="group">
+                    <i class="fas fa-venus-mars"></i>
+                    <select class="form-control" name="gender" required="required">
+                        <option value="" disabled selected>Select Gender</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
+                    </select>
+                </div>
+            </div>
+
 
             <div class=" w3l-form-group">
                 <label>Password:</label>
                 <div class="group">
                     <i class="fas fa-unlock"></i>
-                    <input type="password" class="form-control" name="pwd" placeholder="Password" required="required" />
+                    <input type="password" class="form-control" name="pass" placeholder="Password" required="required" />
                 </div>
             </div>
 
@@ -74,11 +97,52 @@
             
             <button type="submit" name="signup-submit">Sign Up</button>
         </form>
+
+
+
         <p class=" w3l-register-p">Already a member?<a href="index.php" class="register"> Login</a></p>
     </div>
     <footer>
         <p class="copyright-agileinfo"> &copy; </p>
     </footer>
+
+
+    <?php
+ 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db="hostel_db";
+    
+    $conn = mysqli_connect($servername,$username, $password,$db);
+    if (!$conn) {
+    die("Connection failed: "
+        . mysqli_connect_error());
+    }
+    else{
+        echo "Connected successfully";
+        $aadhar=$_POST['aadhar'];
+        $name=$_POST['name'];
+        $dob=$_POST['dob'];
+        $mob=$_POST['mob'];
+        $pass=$_POST['pass'];
+        $gender=$_POST['gender'];
+
+        $sql = "INSERT INTO details(aadhar,name,dob,gender,mob,pass) VALUES ('$aadhar', '$name', '$dob','$gender', '$mob','$pass')";
+
+        if ($conn->query($sql) == TRUE) 
+            {
+            echo "New record created successfully";
+            } 
+        else 
+            {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+        $conn->close();
+        }
+    
+    ?>
 
 </body>
 
